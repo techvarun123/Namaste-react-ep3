acc1e05fc4f726249bbac60ca44bc695e0421b3f
techvarun123 <itzmevarun2003@gmail.com>
2025-11-05 23:01:16 +0530

    2nd commit

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{id:"head"},"hello from js")));
+import React from "react";
+import ReactDOM from "react-dom/client";
+// jsx =>  babel use to transpile the jsxfile into js
+// <h1> </h1> => React.createElement("h1",{},"message")
+ //react element
+// const jsxheading =(<h1 id="heading" className="head" tabIndex="5">hello from jsx </h1>)
+
+// console.log(jsxheading)
+const Title = () => (
+     <h1>hello</h1>
+);
+const number = <span>2000</span>
+const Fn = () =>(
+    
+    <div className="heading">
+       
+    <h1>{number}sweet helo from jsx 🚀</h1> <Title/></div>  
+)
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(parent);
+
+root.render(<Fn/>);
